73ee549d64ce4931399af9b3f3f22050f38017bd
Cezar Andronache <cezar.andronache@gmail.com>
2025-04-06 19:38:21 +0300

    Phase4: new branch product-removal, new method removal by name in Product manager and main instance

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ pm.add_products(Product("Laptop", 3500, 5))
 pm.add_products(Product("Smartphone", 2000, 20))
 pm.add_products(Product("Tablet", 1500, 15))
 
+print(" Product selection is:")
 pm.show_all_products()  ##-afisarea tuturor produselor
 
 total_inventory = pm.show_total_inventory()             ##-apelarea metodei inventar
 print(f"Total inventory value is {total_inventory}")    ##-afisarea valorii inventarului total
 
+pm.remove_product_by_name("Tablet")  ##eliminare produs dupa nume
+print("\n Product selection, after deletion, is:")
+pm.show_all_products()  ##-afisarea produselor dupa eliminare
 
-    
--- product_manager.py
+++ product_manager.py
@@ -19,4 +19,15 @@ class ProductManager:
 
     def show_total_inventory(self):
         return sum(product.price * product.quantity for product in self.products)
-      
+
+## metoda pentru a elimina produsele dupa nume:   
+    def remove_product_by_name(self, product_name):
+        found = False
+        for product in self.products:
+            if product.name.lower() == product_name.lower():
+                self.products.remove(product)
+                print(f"\n Produsul '{product_name}' a fost eliminat cu succes.")
+                found = True
+                break
+        if not found:
+            print(f"Produsul '{product_name}' nu a fost găsit.")
