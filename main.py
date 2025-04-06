e021e95bb9eaf2089f9939f88ddf9a383f578fdb
Cezar Andronache <cezar.andronache@gmail.com>
2025-04-06 20:08:02 +0300

    phase7- main modif on main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ from product import Product
 from product_manager import ProductManager
 
 pm = ProductManager()
-pm.add_products(Product("Laptop", 3500, 5))
-pm.add_products(Product("Smartphone", 2000, 20))
+pm.add_products(Product("Laptops", 3500, 5))
+pm.add_products(Product("Smartphones", 2000, 20))
 pm.add_products(Product("Tablet", 1500, 15))
 
-print(" Product selection is:")
-pm.show_all_products()  ##-afisarea tuturor produselor
+# print(" Product selection is:")
+# pm.show_all_products()  ##-afisarea tuturor produselor
 
-total_inventory = pm.show_total_inventory()             ##-apelarea metodei inventar
-print(f"Total inventory value is {total_inventory}")    ##-afisarea valorii inventarului total
+# total_inventory = pm.show_total_inventory()             ##-apelarea metodei inventar
+# print(f"Total inventory value is {total_inventory}")    ##-afisarea valorii inventarului total
 
 pm.remove_product_by_name("Tablet")  ##eliminare produs dupa nume
 print("\n Product selection, after deletion, is:")
